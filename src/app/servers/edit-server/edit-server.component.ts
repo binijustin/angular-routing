d4a7a253f529a86cc032d066aac06d9e405e375d
Gallardo, Justin R <justin.r.gallardo@accenture.com>
2019-05-23 21:40:03 +0800

    init

---
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit {
+  server: { id: number, name: string, status: string };
+  serverName = '';
+  serverStatus = '';
+  idParam: number;
+  allowEdit = false;
+
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.idParam = this.route.snapshot.params['id'];
+    this.route.params.subscribe((params: Params) => {
+      this.idParam = params['id'];
+    })
+
+    this.route.queryParams.subscribe((params: Params) => {
+      this.allowEdit = params['allowEdit'] === '1' ? true : false;
+    })
+
+
+
+    this.server = this.serversService.getServer(+this.idParam);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
+  }
+
+}
